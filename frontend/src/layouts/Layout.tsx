74ccc88b212ce19c85d61e701174cfc64e9ead06
arjundevn <arjun.dev.n@gmail.com>
2024-05-29 19:16:33 +0530

    feat: added header,hero and footer components

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
