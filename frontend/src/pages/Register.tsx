5b45bcfa874cf8afbe925c50ee4b8de207b03dc9
arjundevn <arjun.dev.n@gmail.com>
2024-05-31 10:38:45 +0530

    feat: completed connecting register's FE and BE

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from '../api-client'
 
 export type RegisterFormData = {
   firstName: string;
@@ -15,8 +17,18 @@ const Register = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<RegisterFormData>();
+
+  const mutation = useMutation(apiClient.register, {
+    onSuccess:  () => {
+      console.log("Registration successful")
+    },
+    onError: (error:Error) => {
+      console.log(error.message);
+    }
+  })
+
   const onSubmit = handleSubmit((data) => {
-    console.log(data);
+    mutation.mutate(data);
   });
 
   return (
